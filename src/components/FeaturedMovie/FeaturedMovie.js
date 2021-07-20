489d5067f53d72e7c40248d8bd3be1096dad9905
Lucas <dexterfail@hotmail.com>
2021-07-20 10:49:24 -0300

    small fix on header buttons

--- src/components/FeaturedMovie/FeaturedMovie.js
+++ src/components/FeaturedMovie/FeaturedMovie.js
@@ -1,30 +1,30 @@
-import React from 'react';
-import './FeaturedMovie.css';
+import React from "react";
+import "./FeaturedMovie.css";
 
 export default function FeaturedMovie({ item }) {
-    
     let firstDate = new Date(item.first_air_date);
     let genres = [];
     for (let i in item.genres) {
-        genres.push( item.genres[i].name );
+        genres.push(item.genres[i].name);
     }
-    
-    let description = item.overview
-    if(description.length > 200) {
-        description = description.substring(0,200)+'...';
+
+    let description = item.overview;
+    if (description.length > 200) {
+        description = description.substring(0, 200) + "...";
     }
-    
+
     return (
-        <section className="featured" style={{
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-            backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
-        }}>
+        <section
+            className="featured"
+            style={{
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`,
+            }}
+        >
             <div className="featured--vertical">
                 <div className="featured--horizontal">
-                    <div className="featured--name">
-                        {item.original_name}
-                    </div>
+                    <div className="featured--name">{item.original_name}</div>
                     <div className="featured--info">
                         <div className="featured--points">
                             {item.vote_average} pontos
@@ -33,19 +33,30 @@ export default function FeaturedMovie({ item }) {
                             {firstDate.getFullYear()}
                         </div>
                         <div className="featured--seasons">
-                           {item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''}
+                            {item.number_of_seasons} temporada
+                            {item.number_of_seasons !== 1 ? "s" : ""}
                         </div>
                     </div>
-                    <div className="featured--description">
-                        {description}
-                    </div>
+                    <div className="featured--description">{description}</div>
                     <div className="featured--buttons">
-                        <a href={`/watch/${item.id}`} className="featured--watchbutton">▶ Assistir</a>
-                        <a href={`/list/add/${item.id}`} className="featured--mylistbutton">+ Minha Lista</a>
+                        <a
+                            // href={`/watch/${item.id}`}
+                            href={"/"}
+                            className="featured--watchbutton"
+                        >
+                            ▶ Assistir
+                        </a>
+                        <a
+                            // href={`/list/add/${item.id}`}
+                            href={"/"}
+                            className="featured--mylistbutton"
+                        >
+                            + Minha Lista
+                        </a>
                     </div>
                     <div className="featured--genres">
                         <strong>Gêneros:</strong>
-                        {genres.join(', ')}
+                        {genres.join(", ")}
                     </div>
                 </div>
             </div>
